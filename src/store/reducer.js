0c8f722276a5e420390931e48a4675de33c48bd9
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-17 11:22:56 +0100

    installed/created all the basic setup

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+// import someFeatureReducer from "./someFeature/reducer";
+
+function someFeatureReducer(state = "hi", action) {
+  return state;
+}
+
+const reducer = combineReducers({
+  someFeature: someFeatureReducer,
+  // etc...
+});
+
+export default reducer;

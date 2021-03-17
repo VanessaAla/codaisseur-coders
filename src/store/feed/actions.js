630cb32f056c8d2415238db6474693a1d608aa59
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-17 16:22:06 +0100

    fetching data with a thunk

--- src/store/feed/actions.js
+++ src/store/feed/actions.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+
+const API_URL = `https://codaisseur-coders-network.herokuapp.com`;
+
 export function startLoading() {
   return {
     type: "feed/startLoading",
@@ -10,3 +14,34 @@ export function postsFetched(morePosts) {
     payload: morePosts,
   };
 }
+
+export async function fetchNext5Posts(dispatch, getState) {
+  //(dispatch and getState) are my new parameters
+  dispatch(startLoading());
+
+  const offset = getState().feed.posts.length;
+
+  const res = await axios.get(`${API_URL}/posts?offset=${offset}&limit=5`);
+
+  const morePosts = res.data.rows;
+
+  dispatch(postsFetched(morePosts));
+}
+
+//or write them all with a surrounding function
+
+/*export const fetchPosts = () => async (dispatch, getState) => {
+  try {
+    const posts = getState().feed.posts;
+    console.log("getState", getState());
+
+    if (!posts.length) {
+      dispatch(startLoading());
+
+      const response = await axios.get(`${API_URL}/posts`);
+
+      dispatch(postsFetched(response.data.rows));
+    }
+  } catch (e) {
+    console.log(e.message);
+  }*/
--- src/store/feed/selectors.js
+++ src/store/feed/selectors.js
@@ -5,3 +5,5 @@ export function selectFeedLoading(reduxState) {
 export function selectFeedPosts(reduxState) {
   return reduxState.feed.posts;
 }
+
+//export const selectPosts = (reduxState) => reduxState.feed.posts;

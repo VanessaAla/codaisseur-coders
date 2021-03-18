85d00f060b4f684fe7eb04e0afcf1f476575c1a7
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-18 18:55:22 +0100

    added a signup page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PostPage from "./pages/PostPage";
+import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import "./App.css";
 
@@ -9,11 +10,15 @@ function App() {
   return (
     <div>
       <div>
+        <Link to="/signup">Sign up</Link>
+      </div>
+      <div>
         <Link to="/login">Log in</Link>
       </div>
       <Switch>
         {/* more pages to be added here later */}
         <Route path="/post/:id" component={PostPage} />
+        <Route path="/signup" component={SignUpPage} />
         <Route path="/login" component={LoginPage} />
         <Route component={HomePage} />
       </Switch>

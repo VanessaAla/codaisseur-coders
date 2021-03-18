692f6916cc73d235c545df327118d437c715d9e5
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-18 16:56:27 +0100

    created a user slice

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PostPage from "./pages/PostPage";
 import LoginPage from "./pages/LoginPage";
@@ -8,6 +8,9 @@ import "./App.css";
 function App() {
   return (
     <div>
+      <div>
+        <Link to="/login">Log in</Link>
+      </div>
       <Switch>
         {/* more pages to be added here later */}
         <Route path="/post/:id" component={PostPage} />

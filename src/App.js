aacd47931962ac56700c01671a8f52de0ac5c7a9
YOURGITHUBUSERNAME <YOUR@EMAIL.com>
2021-03-18 16:36:30 +0100

    created a login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PostPage from "./pages/PostPage";
+import LoginPage from "./pages/LoginPage";
+import "./App.css";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Switch>
         {/* more pages to be added here later */}
         <Route path="/post/:id" component={PostPage} />
+        <Route path="/login" component={LoginPage} />
         <Route component={HomePage} />
       </Switch>
     </div>
